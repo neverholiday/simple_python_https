d12ac83828ce0c891573a133f446e4ea6c60be2b
neverholiday <neverholiday@github.com>
2022-02-03 09:38:26 +0700

    Add kerberos authentication

---
+++ kerberos_http_authen/server_old.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import os
+
+from flask import Flask
+from flask import request
+from flask import Response
+
+import kerberos
+
+os.environ[ 'KRB5_KTNAME' ] = '/home/neverholiday/work/git_repos/kerberos_http_authen/test_alhambra_local.keytab'
+
+app = Flask( __name__ )
+
+ServiceName = 'HTTP@test.alhambra.local'
+
+
+def initkerberosAuthen():
+	principalName = kerberos.getServerPrincipalDetails( 'HTTP', 'test.alhambra.local' )
+	print( 'principalName = {}'.format( principalName ) )
+
+def _unauthorize():
+	return Response('Unauthorized', 401, {'WWW-Authenticate': 'Negotiate'})
+
+@app.route( '/' )
+def main():
+	return "Who are you ?"
+
+@app.route( '/auth' )
+def authen():
+	header = request.headers.get( "Authorization" )
+	# print( f'header = {header}' )
+	# print( '========================================================' )
+	if not header:
+		return _unauthorize()
+	else:
+		token = ''.join(header.split()[1:])
+		rc, state = kerberos.authGSSServerInit(ServiceName)
+		rc = kerberos.authGSSServerStep(state, token)
+
+		kerberosToken = kerberos.authGSSServerResponse(state)
+		user = kerberos.authGSSServerUserName(state)
+
+		if state:
+			kerberos.authGSSServerClean(state)
+		
+		return Response( user, 200, { 'WWW-Authenticate' : 'negotiate {}'.format( kerberosToken ) } )
+
+
+if __name__ == '__main__':
+	initkerberosAuthen()
+	app.run( host='0.0.0.0', port=1111 )
---
+++ kerberos_http_authen/test_elk_krb5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import kerberos
+import requests
+
+
+KerberosPrinciple = 'HTTP@test.alhambra.local'
+
+
+returnCode, krb_context = kerberos.authGSSClientInit( KerberosPrinciple )
+kerberos.authGSSClientStep( krb_context, "" )
+negotiateDetail = kerberos.authGSSClientResponse( krb_context )
+
+headers = {"Authorization": "Negotiate " + negotiateDetail}
+
+response = requests.get( 'https://test.alhambra.local:9200/', verify=False, headers=headers )
+# response = requests.get( 'https://test.alhambra.local:9200/', verify=False )
+print( response )
+
